7ae8f3ecbc6870dc451cb0104b4a233000cac1c7
David <david226815@gmail.com>
2018-06-27 02:41:44 +0530

    first commit

--- fynd_assignment/settings.py
+++ fynd_assignment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z2(7=&w3o8^y#3txni8-k8-^h+oasqw!l$rrk^mqpal9cugom8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'peaceful-island-88973.herokuapp']
+ALLOWED_HOSTS = ['127.0.0.1', 'peaceful-island-88973.herokuapp', 'localhost']
 
 # Application definition
 INSTALLED_APPS = (
