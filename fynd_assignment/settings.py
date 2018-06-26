e6f70db32ede9f1cf7c412ca0eeedd0f030f1e52
David <david226815@gmail.com>
2018-06-26 23:55:20 +0530

    final comit with setting file as production

--- fynd_assignment/settings.py
+++ fynd_assignment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z2(7=&w3o8^y#3txni8-k8-^h+oasqw!l$rrk^mqpal9cugom8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'vast-taiga-69878.herokuapp']
 
 # Application definition
 INSTALLED_APPS = (
