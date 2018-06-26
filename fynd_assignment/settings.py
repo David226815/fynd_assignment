b81219ba13cc3f4aa43d1f0f5e743d2951a7dab1
David <david226815@gmail.com>
2018-06-27 01:51:45 +0530

    changes in setting sfile

--- fynd_assignment/settings.py
+++ fynd_assignment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z2(7=&w3o8^y#3txni8-k8-^h+oasqw!l$rrk^mqpal9cugom8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'peaceful-island-88973.herokuapp']
 
